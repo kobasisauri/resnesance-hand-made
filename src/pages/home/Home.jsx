a1956bd5604054ec3a03d9e0375593b0142bb614
kobasisauri <kobasisauri@gmail.com>
2023-03-20 20:10:46 +0400

    changed slider styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,6 +14,7 @@ import pic2 from "assets/2.JPG";
 import pic3 from "assets/3.JPG";
 import pic4 from "assets/6.png";
 import pic5 from "assets/5.jpg";
+import zIndex from "@mui/material/styles/zIndex";
 
 const items = [pic0, pic1, pic2, pic3, pic4, pic5];
 
@@ -23,6 +24,7 @@ const Home = () => {
   return (
     <>
       <Swiper
+        style={{ height: "42rem", padding: "1rem 0" }}
         effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
@@ -46,25 +48,56 @@ const Home = () => {
       >
         {items.map((item, i) => (
           <SwiperSlide key={i}>
-            <img src={item} alt="slide_image" />
+            <img
+              src={item}
+              alt="slide_image"
+              style={{ borderRadius: "1rem", height: "38rem", width: "35rem" }}
+            />
           </SwiperSlide>
         ))}
 
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          <div className="swiper-pagination"></div>
+        {/* <div
+          className="slider-controler"
+          style={{
+            position: "absolute",
+            zIndex: "1",
+            width: "100%",
+            top: "0",
+          }}
+        > */}
+        <div
+          className="swiper-button-prev slider-arrow"
+          style={{ left: "3%", color: "white" }}
+        >
+          <ion-icon name="arrow-back-outline"></ion-icon>
         </div>
+        <div
+          className="swiper-button-next slider-arrow"
+          style={{ right: "3% ", left: "auto", color: "white" }}
+        >
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </div>
+        {/* <div className="swiper-pagination" style={{ bottom: "-49%" }}></div> */}
+        {/* </div> */}
       </Swiper>
 
-      <Swiper spaceBetween={100} slidesPerView={3} initialSlide={slideIndex}>
+      <Swiper
+        spaceBetween={100}
+        slidesPerView={6}
+        initialSlide={slideIndex}
+        style={{ height: "200px" }}
+      >
         {items.map((item, i) => (
           <SwiperSlide key={i}>
-            <img src={item} alt="slide_image" />
+            <img
+              src={item}
+              alt="slide_image"
+              style={{
+                height: "200px",
+                width: "200px",
+                borderRadius: "1rem",
+              }}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
