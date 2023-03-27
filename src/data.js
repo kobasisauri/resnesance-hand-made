3de8dbddb88bed39cd80bf15f7ae7658f70da3b9
gega.gagua@zazmic.com <gega.gagua@zazmic.com>
2023-03-27 08:02:34 +0400

    Add json data for shop page

---
+++ src/data.js
@@ -0,0 +1,806 @@
+import ANG10_1 from "assets/shop/ANG_10/1.jpeg";
+import ANG10_2 from "assets/shop/ANG_10/2.jpeg";
+import ANG10_3 from "assets/shop/ANG_10/3.jpeg";
+
+import ANGA_1 from "assets/shop/ANG_A/1.jpeg";
+import ANGA_2 from "assets/shop/ANG_A/2.jpeg";
+
+import ANGL_1 from "assets/shop/ANG_L/1.jpeg";
+import ANGL_2 from "assets/shop/ANG_L/2.jpeg";
+import ANGL_3 from "assets/shop/ANG_L/3.jpeg";
+import ANGL_4 from "assets/shop/ANG_L/4.jpeg";
+import ANGL_5 from "assets/shop/ANG_L/5.jpeg";
+
+import ANGLKNOB_1 from "assets/shop/ANG_L_KNOB/1.jpeg";
+import ANGLKNOB_2 from "assets/shop/ANG_L_KNOB/2.jpeg";
+import ANGLKNOB_3 from "assets/shop/ANG_L_KNOBs/3.jpeg";
+
+import ANGLMIX_1 from "assets/shop/ANG_L_MIX/1.jpeg";
+import ANGLMIX_2 from "assets/shop/ANG_L_MIX/2.jpeg";
+import ANGLMIX_3 from "assets/shop/ANG_L_MIX/3.jpeg";
+import ANGLMIX_4 from "assets/shop/ANG_L_MIX/4.jpeg";
+import ANGLMIX_5 from "assets/shop/ANG_L_MIX/5.jpeg";
+import ANGLMIX_6 from "assets/shop/ANG_L_MIX/6.jpeg";
+import ANGLMIX_7 from "assets/shop/ANG_L_MIX/7.jpeg";
+import ANGLMIX_8 from "assets/shop/ANG_L_MIX/8.jpeg";
+import ANGLMIX_9 from "assets/shop/ANG_L_MIX/9.jpeg";
+import ANGLMIX_10 from "assets/shop/ANG_L_MIX/10.jpeg";
+
+import ANGLR_1 from "assets/shop/ANG_L_R/1.jpeg";
+import ANGLR_2 from "assets/shop/ANG_L_R/2.jpeg";
+import ANGLR_3 from "assets/shop/ANG_L_R/3.jpeg";
+import ANGLR_4 from "assets/shop/ANG_L_R/4.jpeg";
+import ANGLR_5 from "assets/shop/ANG_L_R/5.jpeg";
+
+import ANGLS_1 from "assets/shop/ANG_LS/1.jpeg";
+import ANGLS_2 from "assets/shop/ANG_LS/2.jpeg";
+import ANGLS_3 from "assets/shop/ANG_LS/3.jpeg";
+import ANGLS_4 from "assets/shop/ANG_LS/4.jpeg";
+import ANGLS_5 from "assets/shop/ANG_LS/5.jpeg";
+
+import ARES_1 from "assets/shop/ARES/1.jpeg";
+import ARES_2 from "assets/shop/ARES/2.jpeg";
+import ARES_3 from "assets/shop/ARES/3.jpeg";
+import ARES_4 from "assets/shop/ARES/4.jpeg";
+import ARES_5 from "assets/shop/ARES/5.jpeg";
+
+import ARESBWHITE_1 from "assets/shop/ARES_B_White/1.jpeg";
+import ARESBWHITE_2 from "assets/shop/ARES_B_White/2.jpeg";
+import ARESBWHITE_3 from "assets/shop/ARES_B_White/3.jpeg";
+import ARESBWHITE_4 from "assets/shop/ARES_B_White/4.jpeg";
+import ARESBWHITE_5 from "assets/shop/ARES_B_White/5.jpeg";
+
+import ARESB_1 from "assets/shop/ARES_B/1.jpeg";
+import ARESB_2 from "assets/shop/ARES_B/2.jpeg";
+import ARESB_3 from "assets/shop/ARES_B/3.jpeg";
+import ARESB_4 from "assets/shop/ARES_B/4.jpeg";
+import ARESB_5 from "assets/shop/ARES_B/5.jpeg";
+
+import ARESC_1 from "assets/shop/ARES_C/1.jpeg";
+import ARESC_2 from "assets/shop/ARES_C/2.jpeg";
+import ARESC_3 from "assets/shop/ARES_C/3.jpeg";
+import ARESC_4 from "assets/shop/ARES_C/4.jpeg";
+import ARESC_5 from "assets/shop/ARES_C/5.jpeg";
+import ARESC_6 from "assets/shop/ARES_C/6.jpeg";
+
+import PANOC_1 from "assets/shop/PANO_C/1.jpeg";
+import PANOC_2 from "assets/shop/PANO_C/2.jpeg";
+import PANOC_3 from "assets/shop/PANO_C/3.jpeg";
+
+import VEGAMG_1 from "assets/shop/VEGA_MG/1.jpeg";
+import VEGAMG_2 from "assets/shop/VEGA_MG/2.jpeg";
+
+import VINOR_1 from "assets/shop/VINO_R/1.jpeg";
+
+import VINORWENGE_1 from "assets/shop/VINO_R_WENGE/1.jpeg";
+import VINORWENGE_2 from "assets/shop/VINO_R_WENGE/2.jpeg";
+import VINORWENGE_3 from "assets/shop/VINO_R_WENGE/3.jpeg";
+
+import WEB_1 from "assets/shop/WEB/1.jpeg";
+import WEB_2 from "assets/shop/WEB/2.jpeg";
+import WEB_3 from "assets/shop/WEB/3.jpeg";
+
+export const data = [
+  {
+    name: "ANG 10",
+    innerTitle: "",
+    tags: ["aluminum", "brass", "G10", "mirror", "satin", "stainless steel"],
+    description: "Purple G10",
+    price: "280-550",
+    images: [ANG10_1, ANG10_2, ANG10_3],
+  },
+  {
+    name: "ANG A",
+    innerTitle: "Wenge Stainless Steel",
+    tags: ["mirror", "satin", "stainless steel"],
+    description: "11 Inches",
+    price: "350-750",
+    images: [ANGA_1, ANGA_2],
+  },
+  {
+    name: "ANG L",
+    innerTitle: "Blue Stainless steel",
+    tags: ["blue", "mirror", "stainless steel"],
+    description: "22 Inches",
+    price: "750-1200",
+    images: [ANGL_1, ANGL_2, ANGL_3, ANGL_4, ANGL_5],
+  },
+  {
+    name: "ANG L KNOB",
+    innerTitle:
+      "Purpleheart Wenge Carbon Fiber Micarta Bubinga Stainless Steel",
+
+    tags: [
+      "bubinga",
+      "carbon fiber",
+      "mirror",
+      "purpleheart",
+      "stainless steel",
+      "wenge",
+    ],
+    description: "4 Inches",
+    price: "250-420",
+    images: [ANGLKNOB_1, ANGLKNOB_2, ANGLKNOB_3],
+  },
+  {
+    name: "ANG L MIX",
+    innerTitle: "White Purpleheart Wenge Micarta Stainless Steel",
+    tags: ["mirror", "purpleheart", "satin", "stainless steel", "white"],
+    description: "12 Inches",
+    price: "330-550",
+    images: [
+      ANGLMIX_1,
+      ANGLMIX_2,
+      ANGLMIX_3,
+      ANGLMIX_4,
+      ANGLMIX_5,
+      ANGLMIX_6,
+      ANGLMIX_7,
+      ANGLMIX_8,
+      ANGLMIX_9,
+      ANGLMIX_10,
+    ],
+  },
+  {
+    name: "ANG L R",
+    innerTitle: "Purpleheart",
+    tags: ["mirror", "purpleheart"],
+    description: "18 Inches",
+    price: "750-1200",
+    images: [ANGLR_1, ANGLR_2, ANGLR_3, ANGLR_4, ANGLR_5],
+  },
+  {
+    name: "ANG LS",
+    innerTitle: "Purpleheart Micarta Carbon Wenge Stainless Steel",
+    tags: ["carbon fiber", "mirror", "purpleheart", "stainless steel", "wenge"],
+    description: "4 Inches",
+    price: "170-420",
+    images: [ANGLS_1, ANGLS_2, ANGLS_3, ANGLS_4, ANGLS_5],
+  },
+  {
+    name: "ARES",
+    innerTitle: "Micarta Brass",
+    tags: ["brass", "micarta", "satin"],
+    description: "12 Inches",
+    price: "470-790",
+    images: [ARES_1, ARES_2, ARES_3, ARES_4, ARES_5],
+  },
+  {
+    name: "ARES B",
+    innerTitle: "Blue White Purpleheart Stainless Steel",
+    tags: ["blue", "mirror", "purpleheart", "stain", "white"],
+    description: "12 Inches",
+    price: "420-770",
+    images: [
+      ARESBWHITE_1,
+      ARESBWHITE_2,
+      ARESBWHITE_3,
+      ARESBWHITE_4,
+      ARESBWHITE_5,
+    ],
+  },
+  {
+    name: "ARES B",
+    innerTitle: "Blue Stainless Steel",
+    tags: ["blue", "mirror", "stainless steel"],
+    description: "12 Inches",
+    price: "420-970",
+    images: [ARESB_1, ARESB_2, ARESB_3, ARESB_4, ARESB_5],
+  },
+  {
+    name: "ARES C",
+    innerTitle: "Catalox Stainless Steel",
+    tags: ["catalox", "mirror", "stainless steel"],
+    description: "12 Inches",
+    price: "420-970",
+    images: [ARESC_1, ARESC_2, ARESC_3, ARESC_4, ARESC_5, ARESC_6],
+  },
+  /////////////////////////////////
+  {
+    name: "ARES GROSS",
+    innerTitle: "White Brass",
+    tags: ["brass", "micarta", "satin", "white"],
+    description: "14 Inches",
+    price: "950-1200",
+    images: [],
+  },
+  {
+    name: "ARES I",
+    innerTitle: "Ice Stainless Steel",
+
+    tags: ["ice", "mirror", "stainless steel"],
+    description: "12 Inches",
+    price: "420-970",
+    images: [],
+  },
+  {
+    name: "ARES MIX",
+    innerTitle: "White Wenge Catalox Black Ice Blue Brass Stainless",
+
+    tags: [
+      "black",
+      "blue",
+      "brass",
+      "catalox",
+      "ice",
+      "micarta",
+      "mirror",
+      "mix",
+      "stainless steel",
+      "white",
+    ],
+    description: "12 Inches",
+    price: "420-970",
+    images: [],
+  },
+  {
+    name: "ARES MW",
+    innerTitle: "White Brass",
+
+    tags: ["brass", "mirror", "stain", "white"],
+    description: "5 Inches",
+    price: "550-650",
+    images: [],
+  },
+  {
+    name: "ARES W",
+    innerTitle: "Wenge Stainless Steel",
+
+    tags: ["mirror", "signature", "stainless steel", "wenge"],
+    description: "12 Inches",
+    price: "420-970",
+    images: [],
+  },
+  {
+    name: "ARES WH",
+    innerTitle: "White Stainless Steel",
+
+    tags: ["mirror", "stainless steel", "white"],
+    description: "12 Inches",
+    price: "420-970",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC ART",
+    innerTitle: "Walnut Ipe Stainless Steel",
+
+    tags: ["ipe", "mirror", "walnut"],
+    description: "9-14 Inches",
+    price: "330-1200",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC AW",
+    innerTitle: "",
+
+    tags: ["mirror", "signature", "stainless steel", "wenge"],
+    description: "12 Inches",
+    price: "400-970",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC B",
+    innerTitle: "Bocote Stainless Steel",
+
+    tags: ["bocote", "satin", "stainless steel"],
+    description: "12 Inches",
+    price: "520-770",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC P",
+    innerTitle: "Purpleheart stainless Steel",
+
+    tags: ["mirror", "purpleheart", "stainless steel"],
+    description: "10 Inches",
+    price: "350-680",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC W",
+    innerTitle: "Wenge Stainless Steel",
+
+    tags: ["mirror", "signature", "stainless steel", "wenge", "White"],
+    description: "12 Inches",
+    price: "400-970",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC WH",
+    innerTitle: "White Stainless Steel",
+
+    tags: ["mirror", "stainless steel", "White"],
+    description: "12 Inches",
+    price: "480-950",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC Z",
+    innerTitle: "",
+
+    tags: ["assimetric", " stainless steel", "zebra wood"],
+    description: "12 Inches",
+    price: "420-770",
+    images: [],
+  },
+  {
+    name: "ASSIMETRIC ZI",
+    innerTitle: "Ziricote Stainless Steel",
+
+    tags: ["satin", "stainless steel", "ziricote"],
+    description: "12 Inches",
+    price: "420-770",
+    images: [],
+  },
+  {
+    name: "B FLY",
+    innerTitle: "White Bronze",
+
+    tags: ["bronze", "mirror", "white"],
+    description: "10 Inches",
+    price: "420-530",
+    images: [],
+  },
+  {
+    name: "BLUES B",
+    innerTitle: "Blues Stainless Steel Brass Bronze Nickel Aluminum",
+
+    tags: [
+      "aluminum",
+      "blue",
+      "brass",
+      "bronze",
+      "mirror",
+      "nickel",
+      "satin",
+      "stainless steel",
+    ],
+    description: "Blue",
+    price: "",
+    images: [],
+  },
+  {
+    name: "CLW",
+    innerTitle: "Purpleheart stainless Steel",
+
+    tags: ["mirror", "purpleheart", "stainless steel"],
+    description: "3 Inches",
+    price: "750-970",
+    images: [],
+  },
+  {
+    name: "CRC",
+    innerTitle: "Wenge Brass",
+
+    tags: ["brass", "mirror", "wenge"],
+    description: "3 Inches",
+    price: "250-550",
+    images: [],
+  },
+  {
+    name: "GENT",
+    innerTitle: "Epoxy Stainless Steel",
+
+    tags: ["epoxy", "stainless steel"],
+    description: "10 Inches",
+    price: "570-650",
+    images: [],
+  },
+  {
+    name: "GROS",
+    innerTitle: "Ipe Stainless Steel",
+
+    tags: ["ipe", "mirror", "stainless steel"],
+    description: "7 Inches",
+    price: "250-330",
+    images: [],
+  },
+  {
+    name: "HEX R",
+    innerTitle: "Wenge Mahagony Stainless Steel",
+
+    tags: ["mahagony", "mirror", "satin", "stainless steel", "wenge"],
+    description: "32 Inches",
+    price: "4200-5000",
+    images: [],
+  },
+  {
+    name: "HEX WG",
+    innerTitle: "White Brass",
+
+    tags: ["brass", "mirror", "white"],
+    description: "9 Inches",
+    price: "320-550",
+    images: [],
+  },
+  {
+    name: "KNOB ICE",
+    innerTitle: "Cocobolo Ice Stainless Steel",
+
+    tags: ["cocobolo", "ice", "mirror", "stainless steel"],
+    description: "Ice, Cocobolo",
+    price: "350",
+    images: [],
+  },
+  {
+    name: "LANGARD BLUES",
+    innerTitle: "Blues Brass",
+
+    tags: ["blue", "brass", "mirror", "stain"],
+    description: "8 Inches",
+    price: "520-950",
+    images: [],
+  },
+  {
+    name: "LANGARD P",
+    innerTitle: "Purpleheart Brass",
+
+    tags: ["brass", "mirror", "purpleheart", "satin"],
+    description: "12 Inches",
+    price: "790-1150",
+    images: [],
+  },
+  {
+    name: "LEGAN",
+    innerTitle: "Ice",
+
+    tags: ["ice", "mirror", "stainless steel"],
+    description: "14″ mirror polished",
+    price: "1200",
+    images: [],
+  },
+  {
+    name: "LOOP",
+    innerTitle: "Brass Wenge",
+
+    tags: ["brass", "knob", "mirror", "signature", "wenge"],
+    description: "Brass Wenge",
+    price: "550-750",
+    images: [],
+  },
+  {
+    name: "LV",
+    innerTitle: "Brass Wenge",
+
+    tags: ["brass", "lignum vitae", "mirror", "satin"],
+    description: "12 Inches. Beautiful Lignum Vitae. Set of two",
+    price: "1470",
+    images: [],
+  },
+  {
+    name: "M ROOM",
+    innerTitle: "Brass Cocobolo",
+
+    tags: ["brass", "cocobolo"],
+    description: "",
+    price: "550",
+    images: [],
+  },
+  {
+    name: "MACASSAR",
+    innerTitle: "Macassar",
+
+    tags: ["macassar", "satin", "stainless steel"],
+    description: "",
+    price: "370-420",
+    images: [],
+  },
+  {
+    name: "MIX",
+    innerTitle: "Mix",
+
+    tags: ["bronze", "stainless steel"],
+    description: "",
+    price: "580-950",
+    images: [],
+  },
+  {
+    name: "PANO C",
+    innerTitle: "Cocobolo Stainless Steel",
+    tags: [
+      "bookmatched",
+      "cocobolo",
+      "one of a kind",
+      "satin",
+      "stainless steel",
+    ],
+    description: "16″ Bookmatched Cocobolo, One Of a Kind, Not Finished",
+    price: "3000",
+    images: [PANOC_1, PANOC_2, PANOC_3],
+  },
+  {
+    name: "QUAD S",
+    innerTitle: "Stainless Micarta",
+
+    tags: ["micarta", "mirror", "stainless steel"],
+    description: "",
+    price: "290-350",
+    images: [],
+  },
+  {
+    name: "QUAD WG",
+    innerTitle: "White Gray Black",
+
+    tags: [],
+    description: "",
+    price: "270-550",
+    images: [],
+  },
+  {
+    name: "RING",
+    innerTitle: "Ice White",
+
+    tags: ["aluminum", "ice", "nickel", "stainless steel", "white"],
+    description: "",
+    price: "150-470",
+    images: [],
+  },
+  {
+    name: "RING B",
+    innerTitle: "Wenge Ice",
+
+    tags: ["aluminum", "nickel", "wenge"],
+    description: "",
+    price: "270-750",
+    images: [],
+  },
+  {
+    name: "RING BM",
+    innerTitle: "Brass Micarta",
+
+    tags: ["brass", "micarta", "mirror"],
+    description: "",
+    price: "210-550",
+    images: [],
+  },
+  {
+    name: "RING ICE",
+    innerTitle: "Micarta white ice",
+
+    tags: ["aluminum", "ice", "micarta", "mirror", "white"],
+    description: "",
+    price: "150-470",
+    images: [],
+  },
+  {
+    name: "RING W",
+    innerTitle: "Micarta White Ice Blue Black",
+
+    tags: [
+      "aluminum",
+      "black",
+      "blue",
+      "ice",
+      "micarta",
+      "mirror",
+      "nickel",
+      "stainless steel",
+      "wenge",
+      "white",
+    ],
+    description: "",
+    price: "150-470",
+    images: [],
+  },
+  {
+    name: "RND",
+    innerTitle: "Bubinga Wenge Stainless",
+
+    tags: ["bubinga", "satin", "stainless steel", "wenge"],
+    description: "12 Inches",
+    price: "250-570",
+    images: [],
+  },
+  {
+    name: "ROUND R",
+    innerTitle: "Cocobolo Bubinga",
+
+    tags: ["bubinga", "cocobolo", "mirror", "satin"],
+    description: "32 Inches",
+    price: "2000-3000",
+    images: [],
+  },
+  {
+    name: "ROY",
+    innerTitle: "Aluminum Micarta Bubinga",
+
+    tags: ["aluminum", "bubinga", "micarta", "mirror", "satin"],
+    description: "",
+    price: "250-350",
+    images: [],
+  },
+  {
+    name: "SCR",
+    innerTitle: "Aluminum Micarta Bubinga",
+
+    tags: ["knob", "mirror", "purpleheart", "stainless steel"],
+    description: "",
+    price: "400-970",
+    images: [],
+  },
+  {
+    name: "U",
+    innerTitle: "Purpleheart Redheart",
+
+    tags: ["mirror", "purpleheart", "satin", "stainless steel"],
+    description: "",
+    price: "370-550",
+    images: [],
+  },
+  {
+    name: "U",
+    innerTitle: "White Purpleheart",
+
+    tags: ["mirror", "purpleheart", "white"],
+    description: "",
+    price: "370-550",
+    images: [],
+  },
+  {
+    name: "U W",
+    innerTitle: "Wenge Stainless Steel",
+
+    tags: ["mirror", "signature", "stainless steel", "wenge"],
+    description: "10 Inches",
+    price: "380-740",
+    images: [],
+  },
+  {
+    name: "U R",
+    innerTitle: "",
+
+    tags: ["cocobolo", "damascus", "stainless steel"],
+    description: "14 Inches. Beautiful Cocobolo",
+    price: "",
+    images: [],
+  },
+  {
+    name: "VEGA 1",
+    innerTitle: "Rosewood Purpleheart Wenge Micarta",
+
+    tags: ["micarta", "purpleheart", "rosewood", "stainless steel", "wenge"],
+    description: "10 Inches",
+    price: "370-550",
+    images: [],
+  },
+  {
+    name: "VEGA 2",
+    innerTitle: "Olive Wood",
+
+    tags: ["olive wood", "satin", "stainless steel"],
+    description: "10 Inches",
+    price: "550-750",
+    images: [],
+  },
+  {
+    name: "VEGA AL",
+    innerTitle: "Purpleheart White Aluminum",
+
+    tags: ["aluminum", "mirror", "purpleheart", "white"],
+    description: "9 Inches",
+    price: "250-1200",
+    images: [],
+  },
+  {
+    name: "VEGA ALS",
+    innerTitle: "Aluminum Stainless Steel",
+
+    tags: ["aluminum", "satin", "stainless steel"],
+    description: "11 Inches",
+    price: "750-950",
+    images: [],
+  },
+  {
+    name: "VEGA B",
+    innerTitle: "Bocote Macassar",
+
+    tags: [
+      "bocote",
+      "bookmatched",
+      "mirror",
+      "one of a kind",
+      "satin",
+      "stainless steel",
+    ],
+    description: "12 Inches. Bookmatched Bocote",
+    price: "580-750",
+    images: [],
+  },
+  {
+    name: "VEGA B",
+    innerTitle: "Bubinga",
+
+    tags: ["bubinga", "mirror", "satin", "stainless steel"],
+    description: "12 Inches",
+    price: "580-750",
+    images: [],
+  },
+  {
+    name: "VEGA BC",
+    innerTitle: "Bocote",
+
+    tags: ["bocote", "bookmatched", "satin", "stainless steel", "wine room"],
+    description: "12 Inches. Bookmatched Bocote",
+    price: "580-750",
+    images: [],
+  },
+  {
+    name: "VEGA MG",
+    innerTitle: "Wenge",
+    tags: ["mirror", "satin", "signature", "stainless steel", "wenge"],
+    description: "12 Inches. SIGNATURE series, beautiful Wenge",
+    price: "840-950",
+    images: [VEGAMG_1, VEGAMG_2],
+  },
+  {
+    name: "VEGA O",
+    innerTitle: "Ipe",
+
+    tags: ["ipe", "mirror", "stainless steel"],
+    description: "7 Inches",
+    price: "270-350",
+    images: [],
+  },
+  {
+    name: "VEGA P",
+    innerTitle: "Purpleheart",
+
+    tags: ["bocote", "walnut"],
+    description: "12 Inches",
+    price: "450-970",
+    images: [],
+  },
+  {
+    name: "VEGA R",
+    innerTitle: "White Bocote",
+
+    tags: ["mirror", "satin", "stainless steel", "wenge", "white", "wine room"],
+    description:
+      "35 Inches long. Very elegant handle for kitchen appliance, pantry, wine room, closet etc",
+    price: "2800-3500",
+    images: [],
+  },
+
+  {
+    name: "VEGA R MIX",
+    innerTitle: "White Wenge Ipe",
+
+    tags: ["ipe", "wenge", "white"],
+    description: "",
+    price: "570-970",
+    images: [],
+  },
+  {
+    name: "VEGA W",
+    innerTitle: "Wenge",
+
+    tags: ["mirror", "satin", "signature", "stainless steel", "wenge"],
+    description: "12 Inches. SIGNATURE series",
+    price: "680-1070",
+    images: [],
+  },
+  {
+    name: "VINO R",
+    innerTitle: "Cocobolo",
+    tags: ["cocobolo", "satin", "stainless steel", "wine room"],
+    description: "32 Inches. Beautiful Cocobolo. Perfect for wine room",
+    price: "3000",
+    images: [VINOR_1],
+  },
+  {
+    name: "VINO R",
+    innerTitle: "Wenge",
+    tags: ["satin", "signature", "wenge"],
+    description: "32 Inches. Perfect for wine room",
+    price: "2400-3000",
+    images: [VINORWENGE_1, VINORWENGE_2, VINORWENGE_3],
+  },
+  {
+    name: "WEB",
+    innerTitle: "",
+    tags: ["aluminum", "black", "brass"],
+    description: "Black. 10 Inches",
+    price: "550-950",
+    images: [WEB_1, WEB_2, WEB_3],
+  },
+];
