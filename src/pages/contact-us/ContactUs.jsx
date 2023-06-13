93b3232538c52d0989b7e2c60f187e793e1849c5
kobasisauri <kobasisauri@gmail.com>
2023-06-13 15:14:01 +0400

    Add contact us

--- src/pages/contact-us/ContactUs.jsx
+++ src/pages/contact-us/ContactUs.jsx
@@ -4,9 +4,15 @@ import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import styles from "./ContactUs.module.scss";
 
+const initialFormData = Object.freeze({
+  name: "",
+  email: "",
+  message: "",
+});
+
 const ContactUs = () => {
   let color = JSON.parse(localStorage.getItem("theme"));
-
+  const [data, setData] = useState(initialFormData);
   const [theme, setTheme] = useState("#181717");
 
   useEffect(() => {
@@ -34,6 +40,33 @@ const ContactUs = () => {
     },
   };
 
+  const handleSubmit = (values) => {
+    values.preventDefault();
+
+    let formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("email", data.email);
+    formData.append("message", data.message);
+
+    fetch("https://rennaisance-backend.vercel.app/", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => console.log(res))
+      .then((message) => {
+        // console.log(1);
+      });
+  };
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+
+      // Trimming any whitespace
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
   return (
     <div className={styles.container}>
       <h2>CONTACT US</h2>
@@ -51,12 +84,15 @@ const ContactUs = () => {
         </div>
 
         <div className={styles["inner-wrapper"]}>
-          <form className={styles.form}>
+          <form onSubmit={handleSubmit} className={styles.form}>
             <TextField
               id="outlined-basic"
               label="Name*"
               variant="outlined"
               sx={inputStyle}
+              name="name"
+              // value={data.name}
+              onChange={handleChange}
             />
 
             <TextField
@@ -64,14 +100,20 @@ const ContactUs = () => {
               label="E-Mail*"
               variant="outlined"
               sx={inputStyle}
+              name="email"
+              // value={data.email}
+              onChange={handleChange}
             />
 
             <TextField
               id="outlined-multiline-static"
+              name="message"
               label="Message*"
               multiline
               rows={3}
               sx={inputStyle}
+              // value={data.message}
+              onChange={handleChange}
             />
 
             <div className={styles["button-wrrapper"]}>
